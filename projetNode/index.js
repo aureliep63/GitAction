c349d331f08e78008cc5d19cf11c56c765bf364b
aureliep63 <aurelie.pedro.r@gmail.com>
2025-05-12 14:12:53 +0200

    add artef

--- projetNode/index.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, Node.js !");
-// math.js
-function addition(a, b) {
-  return a + b;
-}
-
-module.exports = { addition };
--- projetNode/index.test.js
+++
@@ -1,6 +0,0 @@
-// math.test.js
-const { add } = require('./math');
-
-test('addition de 2 + 3 = 5', () => {
-  expect(add(2, 3)).toBe(5);
-});
